667d19c42bd6598301d61929f654e601bc30bde9
angela <a.napolano@studenti.unisa.it>
2026-05-25 16:07:42 +0200

    subtask.4

--- task.py
+++ task.py
@@ -53,7 +53,15 @@ def calcola_limite(espressione: str, variabile: str, punto: str) -> sympy.Expr:
 
 def calcola_polinomio_taylor(espressione: str, variabile: str, punto: float, ordine: int) -> sympy.Expr:
     """Sub-task 4: Calcolare una Serie di Taylor."""
-    pass
+    import sympy as sp
+    var = sp.symbols(variabile)
+    expr = sp.sympify(espressione)
+    pt = sp.Float(punto)
+
+    serie = sp.series(expr, var, pt, ordine + 1)
+
+    return serie.removeO()
+
 
 def risolvi_sistema_lineare(eq1: str, eq2: str, var1: str, var2: str) -> Dict[sympy.Symbol, sympy.Expr]:
     """Sub-task 5: Risolvere un Sistema Lineare."""
